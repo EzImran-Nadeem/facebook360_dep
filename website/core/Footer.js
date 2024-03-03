05abb4b5195671bb4f32423adfbb77e01953ad82
Imran Nadeem <156400572+EzImran-Nadeem@users.noreply.github.com>
2024-03-03 12:31:04 +0530

    #28 footer update

--- website/core/Footer.js
+++ website/core/Footer.js
@@ -60,7 +60,7 @@ class Footer extends React.Component {
           <div>
             <h5>More</h5>
             <a href={`${this.props.config.baseUrl}blog`}>Blog</a>
-            <a href="https://github.com/">GitHub</a>
+            <a href="https://facebook.github.io/facebook360_dep/">GitHub</a>
             <a
               className="github-button"
               href={this.props.config.repoUrl}
